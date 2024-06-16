deb72dee3fe22e3f5e2061b163e1546f8ccce900
Jessica <jessica-mss@hotmail.com>
2024-06-16 14:54:14 -0300

    chore: initial configuration

---
+++ src/main/java/br/com/olabibank/olabibank/OlabibankApplication.java
@@ -0,0 +1,13 @@
+package br.com.olabibank.olabibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlabibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlabibankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/olabibank/olabibank/OlabibankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.olabibank.olabibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlabibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
